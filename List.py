885ed59e68ac0a509d19ebb72c874cc3a4d0f325
Anukul Ghosh <anukulghosh264@gmail.com>
2025-08-13 23:39:28 +0600

    list access with for loop and some list operations added

--- List.py
+++ List.py
@@ -6,5 +6,13 @@ my_list.insert(1,50)
 print(my_list)
 my_list.pop()
 print(my_list)
-# del my_list[3]
+del my_list[1]
 print(my_list)
+print(len(my_list))
+
+# access with loop 
+for item in my_list: 
+    print(item)
+
+for i in range(len(my_list)):
+    print(my_list[i])
